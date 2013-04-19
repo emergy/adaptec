3f2266e3a3650644dcaffc476e15b64caf65f612
root <root@edge-7.m77.now.ru>
2013-04-19 17:01:43 +0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+
+arcconf - Adaptec Utilite
+arcinfo - SMART info
+ssdlive.pl - KINGSTON SH103S3 SSD Live Left
---
+++ ssdlive.pl
@@ -0,0 +1,34 @@
+#!/use/bin/env perl
+
+use strict;
+use warnings;
+use XML::Simple;
+use FindBin qw/$RealBin/;
+
+# adaptec utilite
+my $arcconf = "$RealBin/arcconf";
+
+# get device id and SMART Attribute from command attribute
+my ($id, $attribute) = @ARGV;
+unless ($id) {
+	print "Usage: $0 <device id> [SMART Attribute]\n\t" .
+		"\tdefaut SMART Attribute - 0xE7";
+}
+
+# defaut SMART Attribute
+$attribute = '0xE7' unless ($attribute);
+
+
+my $xml;
+
+# read GETSMARTSTATS
+open ARC, "$arcconf GETSMARTSTATS 1 |" or die "Can't open process $arcconf: $!\n";
+while (<ARC>) {
+	next unless /^</;
+	$xml .= $_;
+}
+close ARC;
+
+# XML to hash
+my $smart = XMLin($xml) or die "Can't read XML: $!\n";
+print $smart->{PhysicalDriveSmartStats}->{$id}->{Attribute}->{$attribute}->{normalizedCurrent};
